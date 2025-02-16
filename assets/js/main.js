5ef18ff38aa1c08353f703d823e99315291793f3
Akhtar <akhtar.mohammad@aletheatech.com>
2025-02-16 11:14:02 +0530

    new features added

--- assets/js/main.js
+++ assets/js/main.js
@@ -276,6 +276,7 @@ jQuery(function($) {
 
 }(jQuery));
 
+$('body').append("<div id='whatsapp'><a href='https://wa.me/919849459017/?text=Hi... I have a query'><img src='assets/img/whatsapp.png'></a> </div>")
 
 // function to set a given theme/color-scheme
 function setTheme(themeName) {
@@ -333,6 +334,8 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     });
 });
+
+(function(I, L, T, i, c, k, s) {if(I.iticks) return;I.iticks = {host:c, settings:s, clientId:k, cdn:L, queue:[]};var h = T.head || T.documentElement;var e = T.createElement(i);var l = I.location;e.async = true;e.src = (L||c)+'/client/inject-v2.min.js';h.insertBefore(e, h.firstChild);I.iticks.call = function(a, b) {I.iticks.queue.push([a, b]);};})(window, 'https://cdn-v1.intelliticks.com/prod/common', document, 'script', 'https://app.intelliticks.com', 'uWZjKT7QuZHHY4HLY_c', {});
 
 
 
