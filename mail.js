4068d916c8ce47be297fb67937f00cce65729edb
Manjunath-BV23 <bvmanjunath53@gmail.com>
2025-03-03 22:41:52 +0530

    contact form connected to emailjs

--- mail.js
+++ mail.js
@@ -2,7 +2,7 @@
 
 // Initialize EmailJS
 (function () {
-    emailjs.init("UlAEv1UP5YjEDPsr7"); // Replace with your EmailJS public key
+    emailjs.init("o8btEBb-SUis9yHH8"); // Replace with your EmailJS public key
 })();
 
 document.getElementById("contactForm").addEventListener("submit", function (event) {
@@ -11,6 +11,7 @@ document.getElementById("contactForm").addEventListener("submit", function (even
     // Get form values
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
     const subject = document.getElementById("subject").value;
     const message = document.getElementById("message").value;
 
@@ -18,13 +19,14 @@ document.getElementById("contactForm").addEventListener("submit", function (even
     const templateParams = {
         name: name,
         email: email,
+        phone: phone,
         subject: subject,
         message: message,
     };
 
     // Send email using EmailJS
     emailjs
-        .send("service_bbz1xrl", "template_aga3vq9", templateParams)
+        .send("service_rxkupi5", "template_mfu2nwf", templateParams)
         .then(
             function (response) {
                 console.log("SUCCESS!", response.status, response.text);
